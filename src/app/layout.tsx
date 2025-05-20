3ec9691607db6d6a30c83b1b19522a6e3632ed4a
tolik <tol.kowalev@gmail.com>
2025-05-20 17:32:24 +0300

    bugfix: Fix eslint errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,10 @@ import type { Metadata } from "next";
 import { Comfortaa } from "next/font/google";
 import AppLayout from "@/components/layouts/app-layout";
 
-// import "../../public/icons/style.css";
 import "@/resources/styles/main.css";
 import "./globals.css";
 
-export const comfortaa = Comfortaa({
+const comfortaa = Comfortaa({
   weight: ["400", "500", "600", "700"],
   subsets: ["latin", "cyrillic"],
   variable: "--font-comfortaa",
@@ -39,3 +38,42 @@ export default function RootLayout({
     </>
   );
 }
+
+// import type { Metadata } from "next";
+// import { Comfortaa } from "next/font/google";
+// import AppLayout from "@/components/layouts/app-layout";
+
+// import "@/resources/styles/main.css";
+// import "./globals.css";
+
+// const comfortaa = Comfortaa({
+//   weight: ["400", "500", "600", "700"],
+//   subsets: ["latin", "cyrillic"],
+//   variable: "--font-comfortaa",
+//   display: "swap",
+// });
+
+// export const metadata: Metadata = {
+//   title: "Готуємо з Любов’ю",
+//   description:
+//     "Відкривай смачне натхнення щодня. Ця кулінарна книга — твій стильний онлайн-помічник зі смачними та оригінальними рецептами. Готуй легко завдяки покроковим інструкціям, апетитним фото та зручному інтерфейсу. Готуй із задоволенням. Ділись любов’ю через їжу.",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en" className={comfortaa.className}>
+//       <body
+//         className="text-foreground antialiased light bg-card"
+//         style={{
+//           overflow: "visible!important",
+//         }}
+//       >
+//         <AppLayout>{children}</AppLayout>
+//       </body>
+//     </html>
+//   );
+// }
